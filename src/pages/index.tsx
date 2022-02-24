565a5ed0d0cb75e794aef3d24b3350f7a93d8558
Cleber Silva <cleberfasilva@outlook.com>
2022-02-24 17:52:49 -0300

    Add home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,27 @@
+import Head from 'next/head';
+import Image from 'next/image';
 
-import Head from 'next/head'
+import styles from './home.module.scss';
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Início | ig.news</title>
+        <title>Home | ig.news</title>
       </Head>
 
-      <h1>
-        Hello World
-      </h1>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏🏾 Hey, welcome</span>
+          <h1>News about the <span>React</span> world.</h1>
+          <p>
+            Get access to all the publications <br />
+            <span>for $9.90 month</span>
+          </p>
+        </section>
+
+        <Image src="/images/avatar.svg" alt="Girl coding" width={500} height={500} />
+      </main>
     </>
   )
 }
